fd1bc67402545318409f28c7c59d0af8c0311905
Dillon Rice <drice89@gmail.com>
2020-08-06 00:15:55 -0700

    Get discord bot working

---
+++ discord_bot.py
@@ -0,0 +1,20 @@
+import discord
+import discord_bot_constants
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run(discord_bot_constants.BOT_CONSTANTS["auth_token"])
